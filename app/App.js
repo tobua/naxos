2038597e95354baeed706e35b5b8947c83103e17
Matthias Giger <i@matthiasgiger.com>
2021-10-14 22:54:25 +0200

    feat(component): implement swipe and dot functionality for Intro
    release-npm

--- app/App.js
+++ app/App.js
@@ -17,125 +17,128 @@ import {
   Intro,
 } from 'naxos';
 
-const IntroToggle = () => {
-  const [show, setShow] = useState(false);
-
-  if (!show) {
-    return <Button title="Show Intro" onPress={() => setShow(true)} />;
-  }
-
-  return (
-    <Intro onDone={() => setShow(false)}>
-      <Intro.Slide key="first">
-        <Text>First slide.</Text>
-      </Intro.Slide>
-      <Intro.Slide key="second">
-        <Text>Second slide.</Text>
-      </Intro.Slide>
-      <Intro.Slide key="third">
-        <Text>Third slide.</Text>
-      </Intro.Slide>
-    </Intro>
-  );
-};
-
-export default () => (
-  <SafeAreaView style={styles.screen}>
-    <ScrollView>
-      <Content>
-        <Text large>NavBar</Text>
-      </Content>
-      <NavBar title="naxos" />
-      <NavBar>
-        <NavBar.Left>
-          <Icon name="pointer" direction="left" />
-        </NavBar.Left>
-        <NavBar.Middle>
-          <Text>Hello</Text>
-        </NavBar.Middle>
-        <NavBar.Right>
-          <Icon name="menu" />
-        </NavBar.Right>
-      </NavBar>
-      <Content>
-        <Text large>TabBar</Text>
-      </Content>
-      <TabBar onPress={key => console.log(key)}>
-        <TabBar.Tab key="home">
-          <Text>Home</Text>
-        </TabBar.Tab>
-        <TabBar.Tab key="trending">
-          <Text>Trending</Text>
-        </TabBar.Tab>
-        <TabBar.Tab key="about">
-          <Text>About</Text>
-        </TabBar.Tab>
-      </TabBar>
-      <Content>
-        <Text large>Button</Text>
-        <Button title="Click me" />
-        <Button>
-          <Text>Click children</Text>
-        </Button>
-      </Content>
-      <Content>
-        <Text large>Icon</Text>
-        <View style={{flexDirection: 'row', gap: 20}}>
-          <Icon name="find" />
-          <Icon name="find" size="small" />
-          <Icon name="find" size="large" />
-        </View>
-      </Content>
-      <Content>
-        <Text large>Tabs</Text>
-        <Tabs labels={['Home', 'Profile', 'About']}>
-          <Text>Home Content</Text>
-          <Text>Profile Content</Text>
-          <Text>About Content</Text>
-        </Tabs>
-      </Content>
-      <Content>
-        <Text large>Input</Text>
-        <Input placeholder="Enter text..." />
-      </Content>
-      <Content>
-        <Text large>Dropdown</Text>
-        <Dropdown options={['Home', 'Profile', 'About']} onChange={() => {}} />
-      </Content>
-      <Content>
-        <Text large>Text</Text>
-        <Text>This is a paragraph.</Text>
-      </Content>
-      <Content>
-        <Text large>AbsoluteButton</Text>
-        <View
-          style={{
-            backgroundColor: Color.highlight,
-            padding: Space.small,
-            flexDirection: 'row',
-            gap: Space.medium,
-          }}>
-          <AbsoluteButton type="close" position="inline" onPress={() => {}} />
-          <AbsoluteButton type="add" position="inline" onPress={() => {}} />
-          <AbsoluteButton type="back" position="inline" onPress={() => {}} />
-        </View>
-      </Content>
-      <Content>
-        <Text large>Intro</Text>
-        <IntroToggle />
-      </Content>
-    </ScrollView>
-  </SafeAreaView>
-);
-
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
-  title: {
-    fontSize: 50,
-  },
-  subtitle: {
-    fontSize: 15,
-  },
 });
+
+export default () => {
+  const [showInto, showIntro] = useState(false);
+
+  if (showInto) {
+    return (
+      <SafeAreaView style={styles.screen}>
+        <Intro onDone={() => showIntro(false)}>
+          <Intro.Slide key="first">
+            <Content>
+              <Text>First slide.</Text>
+            </Content>
+          </Intro.Slide>
+          <Intro.Slide key="second">
+            <Content>
+              <Text>Second slide.</Text>
+            </Content>
+          </Intro.Slide>
+          <Intro.Slide key="third">
+            <Content>
+              <Text>Third slide.</Text>
+            </Content>
+          </Intro.Slide>
+        </Intro>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.screen}>
+      <ScrollView>
+        <Content>
+          <Text large>NavBar</Text>
+        </Content>
+        <NavBar title="naxos" />
+        <NavBar>
+          <NavBar.Left>
+            <Icon name="pointer" direction="left" />
+          </NavBar.Left>
+          <NavBar.Middle>
+            <Text>Hello</Text>
+          </NavBar.Middle>
+          <NavBar.Right>
+            <Icon name="menu" />
+          </NavBar.Right>
+        </NavBar>
+        <Content>
+          <Text large>TabBar</Text>
+        </Content>
+        <TabBar onPress={key => console.log(key)}>
+          <TabBar.Tab key="home">
+            <Text>Home</Text>
+          </TabBar.Tab>
+          <TabBar.Tab key="trending">
+            <Text>Trending</Text>
+          </TabBar.Tab>
+          <TabBar.Tab key="about">
+            <Text>About</Text>
+          </TabBar.Tab>
+        </TabBar>
+        <Content>
+          <Text large>Button</Text>
+          <Button title="Click me" />
+          <Button>
+            <Text>Click children</Text>
+          </Button>
+        </Content>
+        <Content>
+          <Text large>Icon</Text>
+          <View style={{flexDirection: 'row', gap: 20}}>
+            <Icon name="find" />
+            <Icon name="find" size="small" />
+            <Icon name="find" size="large" />
+          </View>
+        </Content>
+        <Content>
+          <Text large>Tabs</Text>
+          <Tabs labels={['Home', 'Profile', 'About']}>
+            <Text>Home Content</Text>
+            <Text>Profile Content</Text>
+            <Text>About Content</Text>
+          </Tabs>
+        </Content>
+        <Content>
+          <Text large>Input</Text>
+          <Input placeholder="Enter text..." />
+        </Content>
+        <Content>
+          <Text large>Dropdown</Text>
+          <Dropdown
+            options={['Home', 'Profile', 'About']}
+            onChange={() => {}}
+          />
+        </Content>
+        <Content>
+          <Text large>Text</Text>
+          <Text>This is a paragraph.</Text>
+        </Content>
+        <Content>
+          <Text large>AbsoluteButton</Text>
+          <View
+            style={{
+              backgroundColor: Color.highlight,
+              padding: Space.small,
+              flexDirection: 'row',
+              gap: Space.medium,
+            }}>
+            <AbsoluteButton type="close" position="inline" onPress={() => {}} />
+            <AbsoluteButton type="add" position="inline" onPress={() => {}} />
+            <AbsoluteButton type="back" position="inline" onPress={() => {}} />
+          </View>
+        </Content>
+        <Content>
+          <Text large>Intro</Text>
+          <Button title="Show Intro" onPress={() => showIntro(true)} />
+        </Content>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
--- component/Intro.js
+++ component/Intro.js
@@ -1,17 +1,49 @@
 // @flow
-import React, { Children, useState, useMemo } from 'react'
+import React, { Children, useState, useMemo, useRef } from 'react'
 import type ReactNode from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
-import { Color, Font, Button, mergeStyles } from '..'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  PanResponder,
+  Dimensions,
+  Animated,
+  Easing,
+} from 'react-native'
+import { Color, Font, Button, Content, mergeStyles, Space } from '..'
 
 const createBaseStyles = () => ({
   wrapper: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  slides: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  bottom: {
+    padding: Space.medium,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  slide: {
+    flex: 1,
     position: 'absolute',
     top: 0,
-    left: 0,
   },
-  slide: {},
+  dots: {
+    flexDirection: 'row',
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    marginRight: 10,
+  },
 })
+
+const windowWidth = Dimensions.get('window').width
 
 export type Props = {
   children: ReactNode,
@@ -21,16 +53,92 @@ export type Props = {
 
 export const Intro = ({ children, styles, onDone }: Props) => {
   const sheet = useMemo(() => mergeStyles(createBaseStyles(), styles), [styles])
+  const state = useMemo(
+    () => ({
+      index: 0,
+    }),
+    []
+  )
   const [active, setActive] = useState(children[0].key)
+  const [slideIndex, setSlideIndex] = useState(0)
+  const position = useRef(new Animated.Value(0)).current
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        position.setValue(gestureState.dx + state.index * windowWidth * -1)
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        position.setValue(gestureState.dx + state.index * windowWidth * -1)
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        // Next
+        if (
+          gestureState.dx < -(windowWidth / 2) &&
+          state.index < children.length - 1
+        ) {
+          state.index++
+        }
+
+        // Previous
+        if (gestureState.dx > windowWidth / 2 && state.index > 0) {
+          state.index--
+        }
+
+        Animated.timing(position, {
+          toValue: state.index * windowWidth * -1,
+          easing: Easing.ease,
+          useNativeDriver: false,
+          duration: 300,
+        }).start()
+        setTimeout(() => {
+          setSlideIndex(state.index)
+        }, 300)
+      },
+    })
+  ).current
 
   return (
     <View style={sheet.wrapper}>
-      {children.map((child) => (
-        <View key={child.key} style={sheet.slide}>
-          {child}
-          <Button title="Skip" onPress={onDone} />
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[
+          sheet.slides,
+          {
+            width: children.length * windowWidth,
+            transform: [{ translateX: position }],
+          },
+        ]}
+      >
+        {children.map((child, index) => (
+          <View
+            key={child.key}
+            style={[sheet.slide, { left: index * windowWidth }]}
+          >
+            {child}
+          </View>
+        ))}
+      </Animated.View>
+      <View style={sheet.bottom}>
+        <View style={sheet.dots}>
+          {Array.from(Array(children.length)).map((_, index) => (
+            <View
+              key={index}
+              style={[
+                sheet.dot,
+                {
+                  backgroundColor: slideIndex === index ? 'black' : 'lightgray',
+                },
+              ]}
+            />
+          ))}
         </View>
-      ))}
+        <Button
+          title={slideIndex === children.length - 1 ? 'Done' : 'Skip'}
+          onPress={onDone}
+        />
+      </View>
     </View>
   )
 }
